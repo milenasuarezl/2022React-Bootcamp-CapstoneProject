7725c1671e80bd46fdb635df82f296131f0615e3
Milena Suarez <asuarez@hugeinc.com>
2022-06-20 17:55:09 -0500

    feat: Create pagination and loading component

---
+++ src/components/Pagination/Pagination.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    display: flex;
+    padding: 1rem 0;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Button = styled.button`
+    border-radius: 12px;
+    padding: 1rem;
+    margin: 1rem;
+`
