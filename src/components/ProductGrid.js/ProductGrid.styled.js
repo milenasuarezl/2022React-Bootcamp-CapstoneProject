ad03c92f37a4fa846bed75b55db23560b3151754
Milena Suarez <asuarez@hugeinc.com>
2022-06-20 17:57:18 -0500

    feat: Refactor product component

--- src/components/ProductGrid.js/ProductGrid.styled.js
+++ src/components/ProductGrid.js/ProductGrid.styled.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Grid = styled.div`
    display: flex;
    flex-wrap: wrap;
    justify-content: center;
    width: 100%;
+`
+
+export const Container = styled.section`
+   display: flex;
+   flex-direction: column ;
 `
